66d734068270d7c9ea917d75aa527ef38e167d9d
Magnetic_Soul <everything_90s@yahoo.com>
2024-11-26 14:57:55 -0700

    making sure

--- Develop-2/server/src/routes/api/weatherRoutes.ts
+++ Develop-2/server/src/routes/api/weatherRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
+
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
